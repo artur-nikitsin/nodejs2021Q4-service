dbe23b697dc592c6b7e383d256c8b31f6a3918ab
Artur Nikitsin <nikitsin.artur@gmail.com>
2021-12-25 12:08:56 +0300

    feat: implement unhandledRejection logger handlers

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -45,3 +45,11 @@ process.on(
     process.exit(1);
   })
 );
+
+process.on(
+  'unhandledRejection',
+  pino.final(logger, (err, finalLogger) => {
+    finalLogger.error(err, 'unhandledRejection');
+    process.exit(1);
+  })
+);
