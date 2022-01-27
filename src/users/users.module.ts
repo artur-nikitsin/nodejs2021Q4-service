28ecadd8a14968637ebd14e56aaa82f308e1c0a4
Artur <nikitsin.artur@gmail.com>
2022-01-28 00:28:25 +0300

    feature: create users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,18 +1,12 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
-import { User } from '../models/users/user.entity';
-import { Role } from '../models/roles/user-roles.entity';
+
 import { UsersController } from './users.controller';
-import { InvitationModule } from '../invitation/invitation.module';
-import { RemindersModule } from '../reminders/reminders.module';
+import User from '../resources/users/user.model';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User, Role]),
-    forwardRef(() => InvitationModule),
-    forwardRef(() => RemindersModule),
-  ],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
