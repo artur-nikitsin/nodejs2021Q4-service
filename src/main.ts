22f454d8c407aedf71971f25d2beb1889710dbf0
Artur <nikitsin.artur@gmail.com>
2022-01-30 13:33:23 +0300

    feature: implement validation pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { InnerDataValidationPipe } from './utils/pipes/innerDataValidation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new InnerDataValidationPipe());
   await app.listen(4000);
 }
 bootstrap();
---
+++ src/utils/pipes/innerDataValidation.pipe.ts
@@ -0,0 +1,37 @@
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+
+@Injectable()
+export class InnerDataValidationPipe implements PipeTransform {
+  async transform(value: never, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      const errorMessages = errors.map((error) => error.constraints);
+      const message = Object.assign({}, ...errorMessages);
+      const errorResponse = {
+        statusCode: 400,
+        error: 'Bad Request',
+        message,
+      };
+      throw new BadRequestException(errorResponse);
+    }
+    return value;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  private toValidate(metatype: Function): boolean {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
